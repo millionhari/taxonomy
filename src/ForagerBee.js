e02b35e31394b3ecb9a69fd9433c10e51dd21b34
millionhari <mikemhari@gmail.com>
2015-02-09 13:26:10 -0800

    Finish taxonomy

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+	HoneyMakerBee.call(this);
+	this.job = 'find pollen';
+	this.canFly = true;
+	this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(){
+	this.treasureChest.push('treasure');
+}
